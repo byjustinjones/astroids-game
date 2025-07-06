0e348ef592650ad14da5aec559d3229041f549cf
lolitsreality <jjryan25@yahoo.com>
2025-07-05 22:33:09 -0500

    added circles

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import constants
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
