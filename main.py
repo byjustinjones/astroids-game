7d637879878ec83fff838795ef532d200184a80a
lolitsreality <jjryan25@yahoo.com>
2025-07-06 21:01:05 -0500

    added player triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import constants
+from player import Player
+from circleshape import CircleShape
 
 
 def main():
@@ -7,11 +9,13 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(constants.SCREEN_WIDTH // 2, constants.SCREEN_HEIGHT // 2, constants.PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
